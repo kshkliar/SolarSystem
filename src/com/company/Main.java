27c0061ad631361776a374357beef15b5d411ec6
moazzamsadiq <moazzamsadiq94@gmail.com>
2021-11-06 19:56:22 +0500

    Solar System Basic Structure is Ready

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        SolarSystem andromeda =new SolarSystem();
+        andromeda.Sun=1;
+        andromeda.planet=2;
+        andromeda.moon=4;
+
+    }
+}
